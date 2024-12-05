7f33569dc61e08a5651a7069fc099eabf8a9779d
Bo.Zhang <admin@Ubuntu.com>
2024-12-05 13:21:30 +0800

    .gitignore .npmrc dist/assets/index-dw-JKPKw.js dist/index.html index.html package-lock.json package.json src/App.tsx vite.config.ts

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
         </Routes>
